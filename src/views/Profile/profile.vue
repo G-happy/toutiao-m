d895be78ba0658c563d4420a9e6fa3d58169c07e
GHL <1569215575@qq.com>
2022-07-31 11:20:34 +0800

    我的页面骨架搭建完毕

--- src/views/Profile/profile.vue
+++ src/views/Profile/profile.vue
@@ -1,7 +1,20 @@
 <template>
   <div>
-    <h2>profile</h2>
-    <Footer></Footer>
+    <!-- header 区域 -->
+    <div>
+      <!-- header --登录 -->
+      <div v-if="isLogin">登录盒子</div>
+      <!-- header --未登录 -->
+      <div v-else>未登录</div>
+    </div>
+
+    <!-- main 区域 -->
+    <div>收藏区域</div>
+
+    <!-- button 区域 -->
+    <van-button type="danger" @click="logout" v-if="isLogin" class="quitLogin"
+      >退出</van-button
+    >
   </div>
 </template>
 
@@ -10,8 +23,36 @@ export default {
   name: 'Profile',
   data() {
     return {}
+  },
+  methods: {
+    logout() {
+      this.$dialog
+        .confirm({
+          title: '标题',
+          message: '弹窗内容'
+        })
+        .then(() => {
+          // on confirm
+          this.$store.commit('SET_TOKEN', {})
+        })
+        .catch(() => {
+          // on cancel
+        })
+    }
+  },
+  computed: {
+    isLogin() {
+      return !!this.$store.state.tokenObj.token
+    }
   }
 }
 </script>
 
-<style></style>
+<style scoped lang="less">
+// 退出按钮
+.quitLogin {
+  width: 60%;
+  height: 0.9rem;
+  border-radius: 0.45rem;
+}
+</style>
